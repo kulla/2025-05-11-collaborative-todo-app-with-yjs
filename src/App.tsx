fd58b9093ec74c7923b671e61ad028a87f9f21ad
Stephan Kulla <git.mail@kulla.me>
2025-05-11 22:48:18 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+const App = () => {
+  return (
+    <main className="prose p-10">
+      <h1>Rsbuild with React</h1>
+      <p>Start building amazing things with Rsbuild.</p>
+    </main>
+  )
+}
+
+export default App
